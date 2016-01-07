786617905dbe7d6331193f55b1bf7a5c42655883
Kaia Tomson <kaiatoms@gmail.com>
2016-01-07 23:32:36 +0200

    paneeli suurus muudetud

--- src/ArticlesUI.java
+++ src/ArticlesUI.java
@@ -1,7 +1,11 @@
 import com.sun.javafx.css.Stylesheet;
+import javafx.scene.image.*;
+import sun.awt.image.ImageWatched;
+import sun.swing.ImageIconUIResource;
 
 
 import java.awt.*;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
@@ -12,20 +16,15 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-import javax.swing.ButtonGroup;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JRadioButton;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
-import static java.awt.Color.WHITE;
-import static java.awt.Color.red;
+import static java.awt.Color.*;
 
 public class ArticlesUI extends JFrame {
 
+    private Font font;
     private static final String[] ARTICLES = new String[] {"der","die","das"};
     private static final String ARTICLES_FILE = "./src/Sonad/sonad.txt";
 
@@ -39,7 +38,12 @@ public class ArticlesUI extends JFrame {
     private int correctAnswers = 0;
     private int cookie = 1;
 
-    private JLabel wordLabel;
+    private JLabel wordLabel, ImageLabel;
+    private File imageFile;
+    private Image image;
+    private ImageIcon icon;
+    private BorderLayout Layout;
+    private Dimension labelSize;
 
     public ArticlesUI() throws IOException {
         setTitle("DerDieDasGame");
@@ -48,10 +52,17 @@ public class ArticlesUI extends JFrame {
         loadArticlesFromFile();
         articlesIterator = wordArticlesPairs.keySet().iterator();
         currentWord = articlesIterator.next();
+        Dimension labelSize = new Dimension();
+        BorderLayout Layout = new BorderLayout();
+        Layout.setHgap(100);
+        Layout.setVgap(100);
 
-        JPanel panel = new JPanel(new BorderLayout());
-        panel.setBorder(new EmptyBorder(400, 700, 300, 100)); // padding
+        labelSize.height = 200;
+        JPanel panel = new JPanel(Layout);
+        //panel.setSize(3500, 3900);
+        panel.setBorder(new EmptyBorder(200, 200, 200, 200)); // padding
         panel.setBackground(WHITE);
+
         wordLabel = new JLabel(currentWord);
         panel.add(wordLabel, BorderLayout.NORTH);
         panel.add(createRadioPanel(), BorderLayout.CENTER);
@@ -60,6 +71,15 @@ public class ArticlesUI extends JFrame {
         pack();
         // center window
         setLocationRelativeTo(null);
+        //Text font ist Verdana, size 24
+        Font font1 = new Font("Arial", Font.PLAIN, 24);
+        wordLabel.setFont(font1);
+        //wordLabel.setAlignmentY(100);
+        File imageFile = new File("./src/Logo_ddd.png");
+        Image image = ImageIO.read(imageFile);
+        ImageIcon icon = new ImageIcon(image);
+        JLabel imageLabel = new JLabel();
+        imageLabel.setIcon(icon);
     }
 
     private void loadArticlesFromFile() throws IOException {
@@ -73,6 +93,7 @@ public class ArticlesUI extends JFrame {
 
     private JPanel createRadioPanel() {
         JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(300, 50));
         ButtonGroup group = new ButtonGroup();
 
         for (String article : ARTICLES) {
@@ -90,6 +111,9 @@ public class ArticlesUI extends JFrame {
         return panel;
     }
 
+
+    //private static final String Image = "./src/Logo_ddd.png";
+    //ImageIconUIResource icon = new ImageIconUIResource(100[];
     private JButton createAnswerButton() {
         JButton button = new JButton("Answer");
         button.addActionListener(new ActionListener() {
@@ -99,7 +123,7 @@ public class ArticlesUI extends JFrame {
                     return; // do nothing
                 }
                 if (selectedArticle.equals(wordArticlesPairs.get(currentWord))) {
-                    JOptionPane.showMessageDialog(mainFrame, "Correct! " + selectedArticle + " " + currentWord);
+                    JOptionPane.showMessageDialog(mainFrame, "Correct! " + selectedArticle + " " + currentWord, " ", JOptionPane.PLAIN_MESSAGE);
                     correctAnswers += cookie;
                     if (articlesIterator.hasNext()) {
                         currentWord = articlesIterator.next();
@@ -107,15 +131,17 @@ public class ArticlesUI extends JFrame {
                         cookie = 1;
                     } else {
                         float tulemus = ((float)correctAnswers/wordArticlesPairs.size()) * 100;
-                        JOptionPane.showMessageDialog(mainFrame, "Result: "+tulemus + " %");
+                        JOptionPane.showMessageDialog(mainFrame, "Result: "+ tulemus + " %");
                     }
                 } else {
-                    JOptionPane.showMessageDialog(mainFrame, "Wrong! Try again");
+                    JOptionPane.showMessageDialog(mainFrame, "Wrong! Try again", " ", JOptionPane.PLAIN_MESSAGE);
                     cookie = 0;
                 }
             }
         });
+
         return button;
     }
+
 
 }
---
+++ src/Sonad/image.java
@@ -0,0 +1,7 @@
+package Sonad;
+
+/**
+ * Created by kaiatomson on 07/01/16.
+ */
+public class image {
+}
